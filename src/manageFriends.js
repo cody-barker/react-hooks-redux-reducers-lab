5262f2021d0555b07d835626fccda616b4dcc308
Cody <codybarker.or@gmail.com>
2023-08-28 10:28:27 -0700

    complete

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,26 +1,14 @@
-const initialState = {friends: []};
-
-const actionAdd = {
-  type: "friends/add",
-  payload: {
-    name: "Chrome Boi",
-    hometown: "NYC",
-    id: 1
-  }
-}
-
-const actionRemove = {
-  type: "friends/remove",
-  payload: 1
-}
-
-export function manageFriends(state = initialState, action) {
+export function manageFriends(state, action) {
   switch (action.type) {
     case "friends/add":
-      return {friends: state.friends.push(action.payload)};
+      return {
+        friends: [...state.friends, action.payload]
+      };
     case "friends/remove":
-      return {friends: state.friends.filter(f) => f.id !== action.payload.id}
+      return {
+        friends: state.friends.filter((f) => f.id !== action.payload)
+      };
     default:
-      return initialState
+      return state;
   }
 }
